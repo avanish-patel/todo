867c0f3adb2725123833592c3e443d429788c602
Avanish Patel <apatel@solstice.com>
2018-09-06 13:37:43 -0500

    changed server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static("./dist/your_app_name"));
+app.use(express.static("./dist/todoapp"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "/dist/your_app_name/index.html"));
+  res.sendFile(path.join(__dirname, "/dist/todoapp/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
