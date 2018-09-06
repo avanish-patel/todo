60531521c63b4ab5187023ec6d385c2e7e393f0f
Avanish Patel <apatel@solstice.com>
2018-09-06 13:16:31 -0500

    first git remote add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  todo: string = "";
+
+  todoArray = [];
+
+  addTodo(todo) {
+    this.todoArray.push(todo);
+    // console.log(this.todo);
+    // console.log(this.todoArray);
+  }
+
+  deleteTodo(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if (todo === this.todoArray[i]) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
 }
